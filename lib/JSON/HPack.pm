4ba0c7b1820d3060ad23f71799e3174447e14e32
Venkatakrishnan Ganesh <superpulse.x@gmail.com>
2012-04-09 16:35:57 +0530

    * Added Makefile.PL * Added tests * Added some POD

--- lib/JSON/HPack.pm
+++ lib/JSON/HPack.pm
@@ -6,18 +6,59 @@ use JSON::Any;
 
 our $VERSION = q(0.0.1);
 
+
+=head1 NAME 
+
+JSON-HPack - A simple and fast JSON packer
+
+=head1 SYNOPSIS
+
+  use JSON::HPack; 
+
+  JSON::HPack->pack( [ 
+    {
+      name => 'Larry Wall',
+      nick  => 'timtowtdi' 
+    }
+  ] );
+
+  # - OR -
+
+  JSON::HPack->dump( [
+    {
+      name => 'Larry Wall',
+      nick  => 'timtowtdi' 
+    }
+  ] )
+
+  # To Unpack
+  JSON::HPack->unpack(
+    [ 2, 'name', 'nick', 'Larry Wall', 'timtowdi' ]
+  )
+
+  # - OR use JSON string directly
+  JSON::HPack->load( $json_string )
+
+=head1 DESCRIPTION
+
+=cut
+
 sub pack {
   my ( $class, $aoh ) = @_;
 
-  my %first = %{ $aoh->[FIRST] };
-  my ( @keys, $key_size ) = ( keys( %first ), scalar( keys( %first ) ) );
+  my %first     = %{ $aoh->[FIRST] };
+  my $key_size  = scalar( keys( %first ) );
+  my @keys      = keys( %first );
 
   [
     $key_size,
     @keys, 
     map {
-      values( %$_ )
-    } @{ $aoh }[ $key_size .. scalar( @$aoh ) ] 
+      my $this = $_;
+      map {
+        $this->{$_}
+      } @keys
+    } @{ $aoh }[ 0 .. ( scalar( @$aoh ) - 1 ) ] 
   ];
 
 }
@@ -32,12 +73,13 @@ sub unpack {
 
   my ( $start, $length ) = ( scalar( @keys ) ) x 2;
 
-  LOOP: while( $start < @$pa ) {
-    my @values = @{ $pa }[ $start .. ( $start + $length ) ];
+  LOOP: while( ( $start + 1 + $length ) <= @$pa ) {
+    my @values = @{ $pa }[ $start + 1 .. ( $start + $length ) ];
+
     my %hash = (
       map {
        $keys[ $_ ] => $values[ $_ ] 
-      } ( 0 .. $length ) 
+      } ( 0 .. ( $length - 1 ) ) 
     );
 
     push( @$results, { %hash } );
